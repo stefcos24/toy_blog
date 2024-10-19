40b8c2f4ae6dfcb6f359806624c41aedcad7f212
Stefan Cosic <stefan22bl@gmail.com>
2024-10-19 14:42:38 +0200

    Added Article Create endpoint and connected with url

--- domain/urls.py
+++ domain/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from .views import (
     DashboardAPIView,
+    ArticleCreateAPIView,
 )
 
-urlpatterns = [path("", DashboardAPIView.as_view(), name="dashboard")]
+urlpatterns = [
+    path("", DashboardAPIView.as_view(), name="dashboard"),
+    path(
+        "article/create/",
+        ArticleCreateAPIView.as_view(),
+        name="article-create",
+    ),
+]
